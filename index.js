a117cbc6d5108b193c5e8c76b1182c6967d8bb82
TJ Holowaychuk <tj@vision-media.ca>
2012-10-17 09:07:24 -0700

    docs

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 
+/**
+ * Expose `DateRange`.
+ */
+
 module.exports = DateRange;
+
+/**
+ * Initialize a new DateRange `from` one date `to` another.
+ *
+ * @param {Date} from
+ * @param {Date} to
+ * @api public
+ */
 
 function DateRange(from, to) {
   if (from) this.from(from);
   if (to) this.to(to);
 }
+
+/**
+ * Set / get from `date`.
+ *
+ * @param {Date} date
+ * @return {Date}
+ * @api public
+ */
 
 DateRange.prototype.from = function(date){
   if (0 == arguments.length) return this._from;
@@ -12,11 +32,25 @@ DateRange.prototype.from = function(date){
   this.normalize();
 };
 
+/**
+ * Set / get to `date`.
+ *
+ * @param {Date} date
+ * @return {Date}
+ * @api public
+ */
+
 DateRange.prototype.to = function(date){
   if (0 == arguments.length) return this._to;
   this._to = date;
   this.normalize();
 };
+
+/**
+ * Normalize the dates.
+ *
+ * @api private
+ */
 
 DateRange.prototype.normalize = function(){
   if (this._from > this._to) {
@@ -26,9 +60,23 @@ DateRange.prototype.normalize = function(){
   } 
 };
 
+/**
+ * Return diff in milliseconds
+ *
+ * @return {Number}
+ * @api public
+ */
+
 DateRange.prototype.diff = function(){
   return this._to - this._from;
 };
+
+/**
+ * Return JSON representation.
+ *
+ * @return {Object}
+ * @api public
+ */
 
 DateRange.prototype.toJSON = function(){
   return {
@@ -36,6 +84,13 @@ DateRange.prototype.toJSON = function(){
     to: this._to
   };
 };
+
+/**
+ * Return string representation.
+ *
+ * @return {String}
+ * @api public
+ */
 
 DateRange.prototype.toString = function(){
   return '[DateRange ' + this._from + ' => ' + this._to + ']';
